8b7ce36695fb8eab594088b44fd283e8b8668250
sckrich <sinukovdan@mail.ru>
2025-10-23 21:19:48 +0300

    fixed url frontend

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -18,7 +18,7 @@ public class DemoApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:3000", "http://127.0.0.1:3000")
+                    .allowedOrigins("http://localhost:3000", "http://127.0.0.1:3000", "https://practice-frontend-sckrich.amvera.io")
                     .allowedMethods("*")
                     .allowedHeaders("*")
                     .allowCredentials(true);
